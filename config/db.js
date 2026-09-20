d59bd0f9c9bc8b0faad823af069705ffa39b5018
Praneeth Sanjay Dasari <praneethsanjay7@gmail.com>
2026-09-20 11:26:20 +0530

    Configure Render deployment

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         (process.env.DOCKER === "true" ? "mongodb://host.docker.internal:27017/interviewPilot" : "mongodb://localhost:27017/interviewPilot");
 
     try {
-        await mongoose.connect(mongoUri);
+        await mongoose.connect(mongoUri, { serverSelectionTimeoutMS: 5000 });
         console.log("mongoDB connected");
     } catch (error) {
         console.log("mongodb connection failed");
